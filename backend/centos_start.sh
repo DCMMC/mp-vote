a67acb40f390a119e9edf1cc5fb382343d49b417
dcmmc <xwt97294597@gmail.com>
2019-05-10 20:07:52 +0800

    update

--- backend/centos_start.sh
+++ backend/centos_start.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH=/usr/local/lib
 pipenv run python3 ./vote/manage.py makemigrations
 pipenv run python3 ./vote/manage.py migrate
 nohup pipenv run python3 manage.py process_tasks &
-nohup pipenv run python3 ./vote/manage.py runserver 0.0.0.0:8080 &
+nohup pipenv run python3 ./vote/manage.py runserver 0.0.0.0:8888 &
