58ec97b085b2b8fb29741912ce3afe6791c90fb4
dcmmc <xwt97294597@gmail.com>
2019-05-14 17:09:36 +0800

    update

--- backend/vote/vote/urls.py
+++ backend/vote/vote/urls.py
@@ -43,6 +43,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 @csrf_exempt
 def admin_login(request):
+    logger.error('###### fuck: ' + str(request))
     if request.method == 'POST':
         data = json.loads(str(request.body, encoding='utf8'))
         username = data.get('username', '')
